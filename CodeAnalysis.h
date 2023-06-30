778bff1af75efd7643f6442ba80e050babc228b5
Shiro <k758573k@gmail.com>
2023-06-30 13:02:44 +0800

    尝试libtooling

---
+++ CodeAnalysis.h
@@ -0,0 +1,8 @@
+//
+// Created by Shiro on 2023/6/30.
+//
+
+#ifndef CODEANALYSIS_CODEANALYSIS_H
+#define CODEANALYSIS_CODEANALYSIS_H
+#include "src/Log.h"
+#endif //CODEANALYSIS_CODEANALYSIS_H
--- main.cpp
+++ main.cpp
@@ -1,38 +1,10 @@
-#include <iostream>
-#include <clang-c/Index.h>
-#include "src/Log.h"
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <clang/Frontend/FrontendActions.h>
+#include "CodeAnalysis.h"
 
-int main()
+int main(int argc, const char **argv)
 {
-  CXIndex index = clang_createIndex(0, 0);
-  CXTranslationUnit unit = clang_parseTranslationUnit(index,
-                                                      "example.c",
-                                                      nullptr,
-                                                      0,
-                                                      nullptr,
-                                                      0,
-                                                      CXTranslationUnit_None);
-  if (unit == nullptr) {
-    LOG("无法解析");
-    exit(-1);
-  }
-  CXCursor cursor = clang_getTranslationUnitCursor(unit);
-  clang_visitChildren(cursor, [](CXCursor c, CXCursor parent, CXClientData client_data) {
-    if (clang_getCursorKind(c) == CXCursor_CallExpr) {
-      auto cursor = clang_getCursorSpelling(c);
-      auto kind = clang_getCursorKindSpelling(clang_getCursorKind(c));
-      LOG("spell: cursor={},kind={}", clang_getCString(cursor), clang_getCString(kind));
-      clang_disposeString(cursor);
-      clang_disposeString(kind);
-    }
-    return CXChildVisit_Recurse;
-  }, nullptr);
-  auto ret = clang_getCursorLocation(cursor);
-  LOG("location:{}", ret.int_data);
-  LOG("location:{}", ret.ptr_data[0]);
-  LOG("location:{}", ret.ptr_data[1]);
-  
-  clang_disposeTranslationUnit(unit);
-  clang_disposeIndex(index);
+  LOG("{}", clang::tooling::runToolOnCode(std::make_unique<clang::SyntaxOnlyAction>(), "class X {};"));
   return 0;
 }
